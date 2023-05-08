17be6b1c13bdc0a3a98edb65e5d91a81db2c0cdb
MonLes <73730090+Monles@users.noreply.github.com>
2023-05-08 04:20:35 +0000

    update

--- 10-Scraping/hackernews.py
+++ 10-Scraping/hackernews.py
@@ -8,10 +8,10 @@ soup = BeautifulSoup(res.text, 'html.parser')
 soup2 = BeautifulSoup(res2.text, 'html.parser')
 
 links = soup.select(
-  '.titleline > a')  #heads up! .storylink changed to .titleline
+    '.titleline > a')  # heads up! .storylink changed to .titleline
 subtext = soup.select('.subtext')
 links2 = soup2.select(
-  '.titleline > a')  #heads up! .storylink changed to .titleline
+    '.titleline > a')  # heads up! .storylink changed to .titleline
 subtext2 = soup2.select('.subtext')
 
 mega_links = links + links2
@@ -19,20 +19,20 @@ mega_subtext = subtext + subtext2
 
 
 def sort_stories_by_votes(hnlist):
-  return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
 
 def create_custom_hn(links, subtext):
-  hn = []
-  for idx, item in enumerate(links):
-    title = item.getText()
-    href = item.get('href', None)
-    vote = subtext[idx].select('.score')
-    if len(vote):
-      points = int(vote[0].getText().replace(' points', ''))
-      if points > 99:
-        hn.append({'title': title, 'link': href, 'votes': points})
-  return sort_stories_by_votes(hn)
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
 
 
 pprint.pprint(create_custom_hn(mega_links, mega_subtext))
